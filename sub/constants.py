9d7afdfa4aee59e6fbf014d17bb8f2701c8c299d
NMaslenko <nvmaslenko@gmail.com>
2020-03-24 04:48:56 +0500

    -- add new constant

--- sub/constants.py
+++ sub/constants.py
@@ -1,5 +1,14 @@
 
-class ConstantsDB:
+class Constants:
 
 	EMPTY_RESULT_OF_SEARCH = 'No result'
-	TABLE_NAME = 'book_of_contacts'
+	TABLE_NAME = 'contacts'
+	DB_PATH = 'sub/database.db'
+
+	NUMBER = 'number'
+	NAME = 'name'
+	ORGANIZATION = 'organization'
+	DIVISION = 'div'
+	JOB_TITLE = 'job_title'
+	PHONE = 'tel'
+	DESCRIPTION = 'info'
