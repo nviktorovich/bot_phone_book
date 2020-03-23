c68d2a537c84f2477be42fd2aa6cd7e7a888ba46
NMaslenko <nvmaslenko@gmail.com>
2020-03-24 04:50:50 +0500

    -- remake methods and add functions for work with database.db, now searching return results no only with one checking string

--- data_base_instructions.py
+++ data_base_instructions.py
@@ -1,5 +1,38 @@
 import sqlite3
-from sub.constants import ConstantsDB
+from sub.constants import Constants as CT
+
+
+def set_cursor():
+	"""
+
+	:return: cursor with database.db
+	"""
+	con = sqlite3.connect(CT.DB_PATH)
+	cursor = con.cursor()
+	return cursor
+
+
+def get_database_list():
+	"""
+
+	:return: list with all rows in database
+	"""
+	sql = f'SELECT * from {CT.TABLE_NAME}'
+	cursor = set_cursor()
+	database_list = cursor.execute(sql)
+	return database_list
+
+
+def get_number():
+	"""
+
+	:return: number of new contact in str-type
+	"""
+	sql = f'SELECT {CT.NUMBER} from {CT.TABLE_NAME}'
+	cursor = set_cursor()
+	num_list = cursor.execute(sql)
+
+	return str(max([int(x[0]) for x in num_list]) + 1)
 
 
 class ContactsBase:
@@ -7,33 +40,35 @@ class ContactsBase:
 	def __init__(self):
 		self.answer = None
 
-	def set_cursor(self):
-		con = sqlite3.connect('base.db')
-		cursor = con.cursor()
-		print("Successfully Connected to SQLite")
-		return cursor
-
-	def find_by_request(self, request):
-		sql = f"SELECT * from {ConstantsDB.TABLE_NAME} WHERE " \
-		      f"name LIKE '%{request}%' or phone LIKE '%{request}%' or descriptions LIKE '%{request}%'"
-		cursor = self.set_cursor()
-		cursor.execute(sql)
-		self.answer = cursor.fetchall()
+	def search_contact_by_request(self, request):
+		answer = []
+		for row in get_database_list():
+			if set(request.split()).issubset(set(' '.join(row[1:]).split())):
+				answer.append(row[1:])
+		cursor = set_cursor()
 		cursor.close()
-		return self.answer if len(self.answer) > 0 else ConstantsDB.EMPTY_RESULT_OF_SEARCH
+		return answer if len(answer) > 0 else f'По запросу {request} не удалось ничего найти'
 
-	def create_new_contact(self, name, phone, descriptions):
-		sql = f"INSERT INTO {ConstantsDB.TABLE_NAME} " \
-		      f"(name, phone, descriptions) values ('{name}', '{phone}', '{descriptions}')"
-		cursor = self.set_cursor()
+	def create_new_contact_with_data(self, number, name, organization, division, job_title, phone, description):
+		sql = f'''INSERT INTO {CT.TABLE_NAME} 
+({CT.NUMBER}, {CT.NAME}, {CT.ORGANIZATION}, {CT.DIVISION}, {CT.JOB_TITLE}, {CT.PHONE}, {CT.DESCRIPTION}) values 
+("{number}", "{name}", "{organization}", "{division}", "{job_title}", "{phone}", "{description}")'''
+		cursor = set_cursor()
 		cursor.execute(sql)
-		cursor.execute("COMMIT")
-		print(f"Record inserted successfully into {ConstantsDB.TABLE_NAME}")
+		cursor.execute('COMMIT')
 		cursor.close()
+
+
+# def remove_contact(self, name, phone, description):
+#	sql =
 
 
 a = ContactsBase()
 # a.create_new_contact('Арсений', '89777666891', 'еще один тестовый контакт')
-print(a.find_by_request('еще один тестовый контакт'))
+# print(a.find_contact_by_request('ШЧ22'))
 
-a.create_new_contact('Олег', '89777555891', 'еще один тестовый контакт')
+# a.create_new_contact('Олег', '89777555891', 'еще один тестовый контакт')
+
+
+#a.create_new_contact_with_data(get_number(), 'Аркадий райкин', 'цирк', '-', 'клоун', '234234', 'простой тест')
+print(a.search_contact_by_request('клоун'))
