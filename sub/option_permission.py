bbdf818b741c799049984dfcab9bb1aa7c8b239e
NMaslenko <nvmaslenko@gmail.com>
2020-03-29 00:38:10 +0500

    work status and add_contact

--- sub/option_permission.py
+++ sub/option_permission.py
@@ -1,5 +1,6 @@
 class Jumper:
 	status = False
+	add_contact = False
 
 	@staticmethod
 	def set_search_permission(setting=False):
@@ -7,3 +8,10 @@ class Jumper:
 			Jumper.status = True
 		else:
 			Jumper.status = False
+
+	@staticmethod
+	def set_add_contact_permission(setting=False):
+		if setting:
+			Jumper.add_contact = True
+		else:
+			Jumper.add_contact = False
