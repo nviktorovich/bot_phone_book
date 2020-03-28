1e73bc15353a2da851f993771388a5dec3ff30f0
NMaslenko <nvmaslenko@gmail.com>
2020-03-29 00:35:43 +0500

    -- add new functions returned tuple with message(new contact data) and args to create this data-contact in database.db

--- user_instructions.py
+++ user_instructions.py
@@ -1,4 +1,4 @@
-import data_base_instructions
+import data_base_instructions as DB
 from sub.constants import Constants as CT
 
 
@@ -25,3 +25,9 @@ def erase_contact_message():
 def add_contact(user_rows):
 	print(search_message())
 
+
+def check_new_contact_message(name, organization, division, job_title, phone, description):
+	return (f'Данные контакта:\nИмя: \t[{name}]\nОрганизация: \t[{organization}]\nПодразделение: \t[{division}]'
+	        f'\nДолжность: \t[{job_title}]\nТелефон: \t[{phone}]\nДополнительно: \t[{description}]\nСохранить?',
+	        (DB.get_number(), name, organization, division, job_title, phone, description)
+	        )
