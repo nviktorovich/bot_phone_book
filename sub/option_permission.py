399d2ebeb0adf1d3012d1fc402d2d8f6f1758c63
NMaslenko <nvmaslenko@gmail.com>
2020-03-30 00:16:40 +0500

    -- add decorators to disable jumpers -- add function to save new contacts

--- sub/option_permission.py
+++ sub/option_permission.py
@@ -1,17 +1,25 @@
 class Jumper:
-	status = False
-	add_contact = False
+	search_contact_status = False
+	add_contact_status = False
+	new_contact_data = ''
 
 	@staticmethod
 	def set_search_permission(setting=False):
 		if setting:
-			Jumper.status = True
+			Jumper.search_contact_status = True
 		else:
-			Jumper.status = False
+			Jumper.search_contact_status = False
 
 	@staticmethod
 	def set_add_contact_permission(setting=False):
 		if setting:
-			Jumper.add_contact = True
+			Jumper.add_contact_status = True
 		else:
-			Jumper.add_contact = False
+			Jumper.add_contact_status = False
+
+	@staticmethod
+	def add_new_contact_data(data):
+		if data:
+			Jumper.new_contact_data = data
+		else:
+			Jumper.new_contact_data = ''
