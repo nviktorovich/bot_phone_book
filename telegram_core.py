02d409569fc154e45c88b2960115dc95fb5a684f
NMaslenko <nvmaslenko@gmail.com>
2020-03-31 00:51:20 +0500

    -- check remove func from db, add decorators with jumpers permission

--- telegram_core.py
+++ telegram_core.py
@@ -7,13 +7,24 @@ from sub.option_permission import Jumper
 bot = telebot.TeleBot(CT.TELERGAM_API)
 
 
-def jumpers_disabler(func):
-	def wrap(*args, **kwargs):
-		func(*args, **kwargs)
-		Jumper.set_search_permission(False)
-		Jumper.set_add_contact_permission(False)
-
-	return wrap
+def set_approve(approve_search=False, approve_add=False, approve_remove=False):
+	def jumpers_disabler(func):
+		def wrap(*args, **kwargs):
+			func(*args, **kwargs)
+			if approve_search:
+				Jumper.set_search_permission(True)
+			if not approve_search:
+				Jumper.set_search_permission(False)
+			if approve_add:
+				Jumper.set_add_contact_permission(True)
+			if not approve_add:
+				Jumper.set_add_contact_permission(False)
+			if approve_remove:
+				Jumper.remove_contact_with_number(True)
+			if not approve_remove:
+				Jumper.remove_contact_with_number(False)
+		return wrap
+	return jumpers_disabler
 
 
 class Keyboards:
@@ -60,15 +71,23 @@ class Keyboards:
 			telebot.types.InlineKeyboardButton(text=CT.CLOSE_ICON, callback_data='close'))
 		return keyboard
 
+	@staticmethod
+	def back_approve_removing():
+		keyboard = telebot.types.InlineKeyboardMarkup()
+		keyboard.row(
+			telebot.types.InlineKeyboardButton(text=CT.BACK_ICON, callback_data='back'),
+			telebot.types.InlineKeyboardButton(text=CT.APPROVE_REMOVING_ICON, callback_data='approve_removing'))
+		return keyboard
+
 
 @bot.message_handler(commands=['start'])
-@jumpers_disabler
+@set_approve()
 def send_welcome(query):
 	bot.send_message(query.chat.id, text=user_instructions.start_message(), reply_markup=Keyboards.search_add_close())
 
 
 @bot.callback_query_handler(func=lambda c: c.data == 'back')
-@jumpers_disabler
+@set_approve()
 def callback_return_welcome(query):
 	bot.edit_message_text(text=user_instructions.start_message(), chat_id=query.message.chat.id,
 	                      message_id=query.message.message_id,
@@ -76,39 +95,41 @@ def callback_return_welcome(query):
 
 
 @bot.callback_query_handler(func=lambda query: query.data == 'close')
-@jumpers_disabler
+@set_approve()
 def callback_worker_exit(query):
 	bot.delete_message(chat_id=query.message.chat.id, message_id=query.message.message_id)
 
 
 @bot.callback_query_handler(func=lambda query: query.data == 'search')
+@set_approve(approve_search=True)
 def callback_search_menu(query):
 	bot.edit_message_text(text=CT.SEARCH_MESSAGE, chat_id=query.message.chat.id, message_id=query.message.message_id,
 	                      reply_markup=Keyboards.back_close())
-	Jumper.set_search_permission(True)
-	Jumper.set_add_contact_permission(False)
 
 
 @bot.message_handler(func=lambda query: Jumper.search_contact_status)
-@jumpers_disabler
+@set_approve()
 def callback_search_result(query):
+	results = data_base_instructions.ContactsBase.search_contact_by_request(query.text)
 	try:
-		for row in data_base_instructions.ContactsBase.search_contact_by_request(query.text):
-			bot.send_message(chat_id=query.chat.id, text=' '.join(row), reply_markup=Keyboards.save_remove())
+		if not results[0]:
+			bot.send_message(chat_id=query.chat.id, text=CT.NOT_FOUND_CONTACT_MESSAGE+results[1])
+		else:
+			for row in results:
+				bot.send_message(chat_id=query.chat.id, text=' '.join(row), reply_markup=Keyboards.save_remove())
 	except Exception:
 		print(Exception)
 
 
 @bot.callback_query_handler(func=lambda query: query.data == 'add')
+@set_approve(approve_add=True)
 def add_new_contact_menu_callback_worker(query):
 	bot.delete_message(chat_id=query.message.chat.id, message_id=query.message.message_id)
 	bot.send_message(text=CT.ADD_CONTACT_MESSAGE, chat_id=query.message.chat.id, reply_markup=Keyboards.back_close())
-	Jumper.set_search_permission(False)
-	Jumper.set_add_contact_permission(True)
 
 
 @bot.message_handler(func=lambda query: Jumper.add_contact_status)
-@jumpers_disabler
+@set_approve()
 def callback_user_input_contact_message(query):
 	try:
 		name, organization, division, job_title, phone, description = query.text.split('\n')
@@ -122,13 +143,32 @@ def callback_user_input_contact_message(query):
 
 
 @bot.callback_query_handler(func=lambda query: query.data == 'save_new_contact')
-@jumpers_disabler
+@set_approve()
 def callback_save_contact_to_database(query: telebot.types.CallbackQuery):
 	bot.delete_message(chat_id=query.message.chat.id, message_id=query.message.message_id)
 	data_base_instructions.ContactsBase.create_new_contact_with_data(*Jumper.new_contact_data)
 	bot.answer_callback_query(callback_query_id=query.id, text=CT.ADD_NEW_CONTACT_TO_ADDRESS_BOOK_SUCCESS_TEXT,
 	                          cache_time=10)
 	Jumper.add_new_contact_data(False)
+
+
+@bot.callback_query_handler(func=lambda query: query.data == 'remove')
+@set_approve(approve_remove=True)
+def callback_remove_contact_menu(query):
+	Jumper.removing_number = query.message.text.split()[0]
+	bot.edit_message_text(text=query.message.text, chat_id=query.message.chat.id,
+	                      message_id=query.message.message_id, reply_markup=Keyboards.back_approve_removing())
+
+
+@bot.callback_query_handler(func=lambda query: query.data == 'approve_removing')
+@set_approve()
+def callback_remove_contact_menu_apply(query):
+	try:
+		data_base_instructions.ContactsBase.remove_contact_with_number(Jumper.removing_number)
+		bot.delete_message(chat_id=query.message.chat.id, message_id=query.message.message_id)
+		bot.answer_callback_query(callback_query_id=query.id, text=CT.ERASE_CONTACT_MESSAGE, cache_time=10)
+	except Exception:
+		print(Jumper.removing_number)
 
 
 
