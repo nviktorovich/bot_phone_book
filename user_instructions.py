233106dcc43f10ff2df445f0a44e92a01f55952b
NMaslenko <nvmaslenko@gmail.com>
2020-04-01 00:53:36 +0500

    -- add new functions returned alert message

--- user_instructions.py
+++ user_instructions.py
@@ -1,13 +1,16 @@
 import data_base_instructions as DB
-from sub.constants import Constants as CT
 
 
-def start_message():
-	return CT.START_MESSAGE
-
-
-def check_new_contact_message(name, organization, division, job_title, phone, description):
+def send_new_contact_message(name, organization, division, job_title, phone, description):
 	return (f'Данные контакта:\nИмя: \t[{name}]\nОрганизация: \t[{organization}]\nПодразделение: \t[{division}]'
 	        f'\nДолжность: \t[{job_title}]\nТелефон: \t[{phone}]\nДополнительно: \t[{description}]\nСохранить?',
 	        (DB.get_number(), name, organization, division, job_title, phone, description)
 	        )
+
+
+def send_not_found_message(request):
+	return f'⚠️ По запросу: "{request}", ничего не удалось найти'
+
+
+def send_many_founded_contact_message(request):
+	return f'⚠️ По запросу: "{request}", больше 20 совпадений, для отображения результатов воспользуйтесь "/start"'
